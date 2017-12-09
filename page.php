4c0cdd2c24189d5389a6d4570e8457d907b02473
kidvan777 <ivankidyaev.ik@gmail.com>
2017-12-09 09:57:21 +0300

    up

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
 
   while(have_posts()) {
     the_post(); ?>
-    <div class="page-banner">
+  <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>);"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
@@ -15,11 +15,10 @@
   </div>
 
   <div class="container container--narrow page-section">
+<!-- Metabox -->
 	<?php
 		$parent_id = wp_get_post_parent_id(get_the_ID());
-
 		if(wp_get_post_parent_id(get_the_ID())){?>
-
 			<div class="metabox metabox--position-up metabox--with-home-link">
       		<p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parent_id); ?>">
       			<i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent_id); ?></a>
@@ -28,11 +27,9 @@
       		</span>
       		</p>
     	</div>
-
 		<?}?>
-
+<!-- end -->
 		<?php
-
 			$testArray = get_pages(array(
 				"child_of"=>get_the_ID()
 			));
