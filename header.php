0ed6a78c8d759cb12ea7bda7cc981da799c2f459
kidvan777 <ivankidyaev.ik@gmail.com>
2017-12-10 23:00:19 +0300

    googleMapApi

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
             <!-- Events -->
             <li  <?php if(get_post_type()=='event' OR is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event');?>">Events</a></li>
             <!-- Campuses -->
-            <li><a href="#">Campuses</a></li>
+            <li <?php if(get_post_type()=='campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus');?>">Campuses</a></li>
             <!-- Blog -->
             <li  <?php if(get_post_type()=='post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog')?>">Blog</a>
             </li>
